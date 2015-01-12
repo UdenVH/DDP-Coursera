2af3eb31c12cc25b79cc580f0fd44e3d23e7f6a9
UdenVH <udenvh@gmail.com>
2015-01-12 22:50:00 +0100

    added Shiny ui and server code

---
+++ server.R
@@ -0,0 +1,69 @@
+#
+# server code for Developing Data Products-project
+#
+# UdenVH, 09/01/2015
+#
+# documentation:
+#  - Scoping (http://rstudio.github.io/shiny/tutorial/#scoping)
+
+#install.packages("shiny")
+#install.packages("ggmap")
+library(shiny)
+library(ggmap)
+
+# load data (run once)
+load("./data/ledenBWB.RData")
+attach(ledenBWB)
+
+# get a map of Belgium and The Netherlands (run once)
+map <- get_map(location = c(lon=5.1189025, lat=51.5258257), zoom = 7)
+
+# Define server logic
+shinyServer(function(input, output) {
+  
+  # Return a number based on the plot-size parameter-value for resizing the plot:
+  plotSize <- reactive({
+    size <- input$plotSize
+    if (size == '') size <- 600
+    size
+  })
+  
+  # Expression that generates a plot of the distribution. The expression
+  # is wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should be automatically 
+  #     re-executed when inputs change
+  #  2) Its output type is a plot 
+  #
+  output$showExpansion <- renderPlot({
+    # subset the ledenBWB Data Frame and keep the members from the year of 
+    # founding the association up to the year provided by the input slider.
+    year <- 2007 + input$years
+    membersSubset <- ledenBWB[jaarLidmaatschap <= year,]
+    
+    # subset on the size of the vineyard provided by the input slider.
+    if (input$size == "s") 
+       membersSubset <- membersSubset[membersSubset$aantalStokken <= 250,]
+    else if (input$size == "l") 
+      membersSubset <- membersSubset[membersSubset$aantalStokken > 250,]
+    # else keep all (don't subset)
+    
+    # subset on the country of origin provided by the checkbox.
+    membersSubset <- membersSubset[membersSubset$landVanHerkomst %in% input$country,]
+
+    # define the caption text:
+    caption <- "Vineyard location and sizes"
+    if (toString(input$country) != "") caption <- paste(caption, "in", toString(input$country))
+    caption <- paste(caption, "(year ", toString(year), ")")
+    
+    # the legend defines the size of the vineyards (make also selectable)
+    legend <- c(1,5,10,50,100,500,1000,5000)
+    
+    # plot the member distribtution (in green: #008B00) on the map:
+    p <- ggmap(map)
+    p <- p + geom_point(mapping = aes(x = lon, y = lat, size = sqrt(aantalStokken)), colour="#FF7300", data = membersSubset, alpha = .75, na.rm = TRUE, show_guide = TRUE)
+    p <- p + scale_size_area(breaks = sqrt(legend), labels = legend, limits=c(1, 120), name = "vineyard size")
+    p + ggtitle(caption)
+  }
+  , width=plotSize, height=plotSize)
+})
---
+++ ui.R
@@ -0,0 +1,74 @@
+#
+# ui code for Coursera "Developing Data Products"-project
+#
+# purpose: Show the expansion of the association "Brabantse Wijnbouwers", a community of hobby grapegrowers in  Belgium and the Netherlands. 
+#
+# run from the directory where this file can be found with:
+# runApp(".")
+#
+# UdenVH, 09/01/2015
+#
+
+#
+# TODO: 
+#  1. presentation, like: http://rpubs.com/albahnsen/model, http://rpubs.com/Debarati/52894
+#
+# setwd("~/Documents/Coursera - Developing Data Products/project/bwb")
+library(shiny)
+
+selection <- c("The Netherlands (NL)" = "NL","Belgium (BE)" = "BE")
+
+# Define UI for application
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Expansion of the association 'Brabantse Wijnbouwers'"),
+  p("The 'Brabantse Wijnbouwers' are a Dutch speaking community of hobby grapegrowers and winemakers 
+    in Belgium and the Netherlands. 
+    The association was founded in 2007 and aims to spread knowledge on cool climate vineyards 
+    and wine making amongst its members and others with an interest in winemaking. Since its founding the association has grown from 
+    7 to almost 350 members.",br(),br(),"You can view the expansion over the years by moving the 'years'-slider, select the member in Belgium and/or The Netherlands with the check-box and select on vineyard-size (measured in number of vines)."),
+  sidebarLayout(
+    # Sidebar with a slider input for years since founding the association.
+    sidebarPanel(
+      sliderInput("years", 
+                  "Years since founding in 2007:",
+                  min = 0,
+                  max = 8, 
+                  value = 0,
+                  step = 1),
+      br(),
+      radioButtons("size", 
+                   label = "Size of the vineyard:", 
+                   choices = c("small (less or equal to 250 vines)" = "s",
+                               "large (more then 250 vines)" = "l",
+                               "all"   = "a"),
+                   selected = "a"
+                   ),
+      br(),
+      checkboxGroupInput("country",
+                         "Countries:",
+                         choices = selection,
+                         selected = selection,
+      ),
+      # force some space between less important imput:
+      br(),
+      hr(),
+      sliderInput("plotSize", 
+                  "Map-size in pixels (adjust to your screen):", 
+                  min = 400,
+                  max = 1100, 
+                  value = 600,
+                  step = 100)
+    ),
+    
+    # Show a plot of the distribution of members over Belgium and the Netherlands as a function of time since founding the association.
+    mainPanel(
+      plotOutput("showExpansion")
+    )
+),
+p(strong("Source:")," http://www.brabantsewijnbouwers.nl",br(),
+  strong("Note:")," the data is the result of a data-mining",br(),
+ "exercise where html-scraping with R was applied",br(),
+ "to the membership-page at this website.")
+))
